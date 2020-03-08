30336619d70c85828a23d75b62a07d2866fe04d6
dhruvsingh14 <42729611+dhruvsingh14@users.noreply.github.com>
2020-03-08 18:52:45 -0400

    Delete wk4.py

--- wk4/wk4.py
+++
@@ -1,9 +0,0 @@
-import urllib.request, urllib.parse, urllib.error
-from bs4 import BeautifulSoup
-
-# opens connection
-fhand = urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
-
-# runs through each line of url
-for line in fhand:
-    print(line.decode().strip())
