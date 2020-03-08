7493ab0b522aaa8bc680583aa9eb391f2a3e4a7a
dhruvsingh14 <42729611+dhruvsingh14@users.noreply.github.com>
2020-03-08 18:53:11 -0400

    Delete 12.5.py

--- wk4/12.5.py
+++
@@ -1,8 +0,0 @@
-# 12.5
-import urllib.request, urllib.parse, urllib.error
-from bs4 import BeautifulSoup
-
-fhand = urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
-
-for line in fhand:
-    print(line.decode().strip())
